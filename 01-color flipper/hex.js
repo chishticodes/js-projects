a9e81277fc8ceed68ac98b46eb5303c51395b3db
chishticodes <chishticodes@gmail.com>
2023-07-03 15:49:30 +0500

    project 1, color flipper completed

--- 01-color flipper/hex.js
+++ 01-color flipper/hex.js
@@ -1,1 +1,18 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function () {
+  document.body.style.backgroundColor = getRandomNumber();
+  color.textContent = getRandomNumber();
+});
+
+function getRandomNumber() {
+  let num = '#';
+  for (let i = 0; i < 6; i++) {
+    let randNum = Math.floor(Math.random() * hex.length);
+    num += hex[randNum];
+  }
+  return num;
+}
