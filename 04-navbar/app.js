8a3d0dd3f2d39a369a2f87366fb4dd7dc0f35b53
chishticodes <chishticodes@gmail.com>
2023-07-04 20:16:01 +0500

    project 4, navbar toggle completed

--- 04-navbar/app.js
+++ 04-navbar/app.js
@@ -1,5 +1,18 @@
-// classList - shows/gets all classes
-// contains - checks classList for specific class
-// add - add class
-// remove - remove class
-// toggle - toggles class
+// selecting elements
+
+const toggle = document.querySelector('.nav-toggle');
+const ul = document.querySelector('.links');
+
+toggle.addEventListener('click', function () {
+  // my code works fine
+
+  // if (ul.classList.contains('show-links')) {
+  //   ul.classList.remove('show-links');
+  // } else {
+  //   ul.classList.add('show-links');
+  // }
+
+  // can be done in a single line
+
+  ul.classList.toggle('show-links');
+});
